85c4b2f31117727722e2618ad2650136898d89b8
SnehaSree93 <jambula.sneha@gmail.com>
2019-05-18 22:28:37 +0530

    FirstCommit

---
+++ JavaHackerRankProgms/src/test/java/StdinAndStdOut.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by lovel on 26-Apr-19.
+ */
+public class StdinAndStdOut {
+
+    public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double y=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + y);
+        System.out.println("Int: " + i);
+
+    }
+}
